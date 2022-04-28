502fba1dda4a197e7e6bed29efa8d5a306cb225a
vasilijgnedko <leonidasway11051989@gmail.com>
2022-04-28 12:10:05 +0300

    high throughput producer

--- src/main/java/com/kafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/kafka/tutorial2/TwitterProducer.java
@@ -59,6 +59,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
+        //high throughput producer
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+
         return new KafkaProducer<>(properties);
     }
 }
